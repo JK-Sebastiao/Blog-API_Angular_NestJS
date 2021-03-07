7d912f02765578d6f935c0d0ae238a9c94f15349
João Kiakumbo <14078333+JK-Sebastiao@users.noreply.github.com>
2021-03-08 00:23:37 +0100

    Update user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,7 +3,7 @@ import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './models/user.entity';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports:[
